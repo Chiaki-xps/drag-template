f366903712c954319daf6ab12f52fe5e83aa9dbd
Chiaki-xps <1490295446@qq.com>
2023-08-24 17:29:10 +0800

    init

---
+++ prettier.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  printWidth: 80, // 行字符长度
+  semi: true, // 末尾分号
+  vueIndentScriptAndStyle: false, //vue脚本和样式缩进
+  singleQuote: true, // 单引号
+  trailingComma: 'all', // 尾随逗号
+  endOfLine: 'lf', // 使用lf换行符
+  useTabs: false, // 使用空格tab
+  tabWidth: 2, // tab键空格数
+  quoteProps: 'consistent-as-needed', // 对象属性要么全部用引号，要么都不用
+  bracketSpacing: true, // 对象括号内空格
+  bracketSameLine: false, // 多行非自闭合HTML元素的 > 放在单独一行
+  arrowParens: 'avoid', // 箭头函数单个参数可省略括号
+  htmlWhitespaceSensitivity: 'strict', // html内文本空格处理
+};
